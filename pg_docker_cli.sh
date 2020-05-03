c820c280b1e9d05caf5afddc729cb4c222e89960
bozzlab <41643434+bozzlab@users.noreply.github.com>
2020-05-03 15:58:58 +0700

    Update pg_docker_cli.sh

--- pg_docker_cli.sh
+++ pg_docker_cli.sh
@@ -1,3 +1,3 @@
-docker run -d --name postgres_db -e POSTGRES_PASSWORD=password -p 54320:5432 postgres:11
+docker run -d --name postgres_db -e POSTGRES_PASSWORD=password  -v $PWD/postgres/data:/var/lib/postgresql/data  -p 54320:5432 postgres:11 
 docker exec -it postgres_db psql -U postgres 
 # docker exec -it postgres_db psql -U postgres  -c "create database master_data"
